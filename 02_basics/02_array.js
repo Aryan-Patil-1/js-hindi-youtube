3d0a334b05ef37e0b320cfeb846b0fa5dd915c13
Aryan <aryan369@gmail.com>
2025-03-19 12:47:35 +0530

    adding comments

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -29,3 +29,10 @@ let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+
+/*
+1. Spread operator( ...arrayName) = to create a shallow copy of an array,to join/concatenate arrays,to add elements to an array.
+2. .flat(infinity) = to flatten infinity level nested arrays.
+3. .from = for converting array-like and objects into arrays.
+4. for creating new arrays from a set of values, regardless of how many values are provided or their types
+*/
