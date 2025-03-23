771cf284068519798d562bffc57f585cc2f7aae4
Aryan <aryan369@gmail.com>
2025-03-23 10:52:30 +0530

    commit changes

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,12 +1,12 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"]
 
 
-// const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
-// } )
+const values = coding.forEach( (item) => {
+    // console.log(item);
+     return item
+ } )
 
-// console.log(values);
+ console.log(values);
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -61,4 +61,9 @@ const books = [
   });
   console.log(getFacultyObj)
     
+
+  getFacultyObj=facultyObj.forEach((item)=>{
+        return item.Year>=2000;
+  }
+   );
   
